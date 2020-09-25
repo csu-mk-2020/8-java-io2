74ccee6021d28d1082127e9c2f9095dbc0f0a598
Arthur <parabellum357@hotmail.com>
2020-09-25 22:00:23 +0500

    Task01 [Саитгалин]

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,10 +1,15 @@
 package com.example.task01;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Task01Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
 
@@ -14,7 +19,26 @@ public class Task01Main {
     }
 
     public static String extractSoundName(File file) throws IOException, InterruptedException {
-        // your implementation here
-        return "sound name";
+        String[] args = {"ffprobe", "-v", "error", "-of", "flat", "-show_format", file.toString()};
+        ProcessBuilder processBuilder = new ProcessBuilder(args)
+                .redirectError(ProcessBuilder.Redirect.INHERIT)
+                .redirectOutput(ProcessBuilder.Redirect.PIPE);
+        Process process = processBuilder.start();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        int exitCode = process.waitFor();
+        if (exitCode != 0) {
+            System.err.println("Process exited with code " + exitCode);
+            return null;
+        }
+        Pattern pattern = Pattern.compile("format\\.tags\\.title=\"(.+)\"");
+        AtomicReference<String> result = new AtomicReference<>();
+        bufferedReader.lines().forEach(line -> {
+            if (result.get() == null) {
+                Matcher matcher = pattern.matcher(line);
+                if (matcher.matches())
+                    result.set(matcher.group(1));
+            }
+        });
+        return result.get();
     }
 }
