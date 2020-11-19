2e3f7f78641dd21a605aa0708d6c1c06cf07baae
redrockstyle <ulowpie@gmail.com>
2020-11-19 21:22:01 +0500

    [Khazov] Task01

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,20 +1,37 @@
 package com.example.task01;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
 
-        /*
+
         System.out.println(extractSoundName(new File("task01/src/main/resources/3727.mp3")));
-        */
+
     }
 
-    public static String extractSoundName(File file) throws IOException, InterruptedException {
+    public static String extractSoundName(File file) throws IOException {
         // your implementation here
-        return "sound name";
+        String[] strings = {"ffprobe", "-v", "error", "-of", "flat", "-show_format", file.toString()};
+        ProcessBuilder processBuilder = new ProcessBuilder(strings)
+                .redirectError(ProcessBuilder.Redirect.INHERIT)
+                .redirectOutput(ProcessBuilder.Redirect.PIPE);
+        Process process = processBuilder.start();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+
+        String str;
+        while ((str = reader.readLine())!=null){
+            if (str.matches("format.tags.title=.*")){
+                return str.replaceFirst("format.tags.title=","").replaceAll("\"","");
+            }
+        }
+        return "";
     }
 }
