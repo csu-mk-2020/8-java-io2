ca147b3ee5584152ea8ba38d0e47a7da15c76d68
Aleksey <alex-kadykov@bk.ru>
2020-12-14 13:46:43 +0500

    task01 [Кадыков]

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -2,6 +2,9 @@ package com.example.task01;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -14,7 +17,26 @@ public class Task01Main {
     }
 
     public static String extractSoundName(File file) throws IOException, InterruptedException {
-        // your implementation here
-        return "sound name";
+        String[] args = {"ffprobe", "-v", "error", "-of", "flat", "-show_format", file.toString()};
+        ProcessBuilder pb = new ProcessBuilder(args).redirectOutput(ProcessBuilder.Redirect.PIPE)
+                .redirectError(ProcessBuilder.Redirect.INHERIT);
+        Process p = pb.start();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+        if (p.waitFor() != 0) {
+            System.out.println("Process failed!");
+            return "";
+        }
+
+        Pattern re = Pattern.compile("format\\.tags\\.title=\"(.*?)\"");
+        while (reader.ready()) {
+            String s = reader.readLine();
+            Matcher m = re.matcher(s);
+            if (m.matches()) {
+                return m.group(1);
+            }
+        }
+
+        return "";
     }
 }
