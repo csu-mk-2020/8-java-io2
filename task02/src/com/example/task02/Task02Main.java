e181d844456da4c800bc7d3a3103654e787783a3
Aleksey <alex-kadykov@bk.ru>
2020-12-14 13:47:20 +0500

    task02 [Кадыков]

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,11 +1,16 @@
 package com.example.task02;
-
+import java.util.Objects;
 import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Task02Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args)  {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
 
@@ -15,9 +20,31 @@ public class Task02Main {
 
     }
 
-    public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
-        // your implementation here
+    public static List<Path> listFiles(Path rootDir) throws IOException {
+        Objects.requireNonNull(rootDir);
+        ArrayList<Path> files = new ArrayList<>();
+        Files.walkFileTree(rootDir, new FileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
+                return FileVisitResult.CONTINUE;
+            }
 
-        return null;
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+                files.add(file);
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFileFailed(Path file, IOException exc) {
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) {
+                return FileVisitResult.CONTINUE;
+            }
+        });
+        return files;
     }
 }
