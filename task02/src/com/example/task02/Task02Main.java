0dd26d6f7ecfa8183bef11f9d165169ef2b4cc99
Astaroth <avm725@gmail.com>
2020-12-25 07:39:16 +0500

    Task02[Myslivko]

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -3,6 +3,12 @@ package com.example.task02;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Objects;
+import java.util.ArrayList;
+import java.nio.file.Files;
+import java.nio.file.FileVisitor;
+import java.nio.file.FileVisitResult;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -15,9 +21,30 @@ public class Task02Main {
 
     }
 
-    public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
+    public static List<Path> listFiles(Path rootDir) throws IOException {
         // your implementation here
 
-        return null;
+        Objects.requireNonNull(rootDir);
+        ArrayList<Path> files = new ArrayList<>();
+        Files.walkFileTree(rootDir, new FileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
+                return FileVisitResult.CONTINUE;
+            }
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+                files.add(file);
+                return FileVisitResult.CONTINUE;
+            }
+            @Override
+            public FileVisitResult visitFileFailed(Path file, IOException exc) {
+                return FileVisitResult.CONTINUE;
+            }
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) {
+                return FileVisitResult.CONTINUE;
+            }
+        });
+        return files;
     }
 }
